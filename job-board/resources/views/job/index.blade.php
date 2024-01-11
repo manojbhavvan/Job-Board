ff3a31176760ad3ce994cea60a857aa6235d9e69
Manoj Bhavvan <manojbhavvan124@gmail.com>
2024-01-11 09:10:24 +0530

    Added Breadcrumbs navigation func

--- job-board/resources/views/job/index.blade.php
+++ job-board/resources/views/job/index.blade.php
@@ -1,5 +1,8 @@
 <x-layout>
     @vite('resources/css/app.css')
+    <x-breadcrumbs class="mb-4"
+        :links="['Jobs' => route('jobs.index')]"/>
+    <x-job-card :$job />
     @foreach ($jobs as $job)
         <x-job-card class="mb-4" :$job>
             <div>
--- job-board/resources/views/job/show.blade.php
+++ job-board/resources/views/job/show.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    @vite('resources/css/app.css')
+    <x-breadcrumbs class="mb-4"
+        :links="['Jobs' => route('jobs.index'), $job->title => '#']"/>
     <x-job-card :$job />
 </x-layout>
