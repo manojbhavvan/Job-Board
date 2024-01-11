636bea4bc430dd1e7a9a4ce1730cfc3b90ec9831
Manoj Bhavvan <manojbhavvan124@gmail.com>
2024-01-11 16:35:10 +0530

    Added Upload CV functionality

--- job-board/resources/views/job_application/create.blade.php
+++ job-board/resources/views/job_application/create.blade.php
@@ -13,7 +13,8 @@
         Your Job Application
       </h2>
   
-      <form action="{{ route('job.application.store', $job) }}" method="POST">
+      <form action="{{ route('job.application.store', $job) }}" method="POST"
+      enctype="multipart/form-data">
         @csrf
         <div class="mb-4">
           <label for="expected_salary"
@@ -21,6 +22,10 @@
           <x-text-input type="number" name="expected_salary" />
         </div>
   
+        <div class="mb-4">
+          <label class="mb-2 block text-sm font-medium text-slate-900">Upload CV</label>
+          <x-text-input type="file" name="cv" />
+        </div>
         <x-button class="w-full">Apply</x-button>
       </form>
     </x-card>
